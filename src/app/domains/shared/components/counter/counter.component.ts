a158ae709b53fac3bad4048d429ee320b87c1937
sebastian <segut10@hotmail.com>
2024-09-10 15:16:31 -0500

    subiendo curso de angular

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,69 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required:true}) duration = 0
+  @Input({required:true}) message = ''
+  counter = signal(0)
+  counterRef: number | undefined
+
+  constructor(){
+    // no async
+    // before render
+    // run one time
+    console.log('Constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    // before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+    const duration = changes['duration']
+    console.log(duration);
+    if(duration && duration.currentValue !== duration.previousValue){
+      this.doSomething()
+    }
+
+  }
+
+  ngOnInit(){
+    // after render
+    // run one time
+    // can you use async, promise, subscribe
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('during ->', this.duration);
+    console.log('message ->', this.message);
+    this.counterRef = window.setInterval(()=>{
+      console.log('Run interval');
+      this.counter.update(statePrev => statePrev + 1)
+    },1000)
+  }
+
+  ngAfterViewInit(){
+    // after render
+    // ask if the children were render
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef)
+  }
+  doSomething(){
+    console.log('Change duration');
+
+
+  }
+}
---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  hideSiteMenu = signal(true);
+  private cartService = inject(CartService);
+  cart = this.cartService.cart;
+  total = this.cartService.total;
+
+  toogleSideMenu() {
+    this.hideSiteMenu.update((prevState) => !prevState);
+  }
+}
